0e657f32c813a4927ac348254a80c97f0c3c4639
shayaanameer97 <rizwana99909@gmail.com>
2025-11-08 22:24:09 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JS Test</title>
+  <script>
+    function sayHello() {
+      alert('Hello from JavaScript!');
+    }
+  </script>
+</head>
+<body>
+  <button onclick="sayHello()">Click Me</button>
+</body>
+</html>
